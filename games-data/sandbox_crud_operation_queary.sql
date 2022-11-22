dd9f0336e8ed5edd205961d41bfaf82c03797360
KingRivera <jose.rivera15@utrgv.edu>
2022-11-22 13:22:54 -0600

    Add game data

---
+++ games-data/sandbox_crud_operation_queary.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (2435657865, 1, 'extravagante');
+
+--UPDATE sandbox SET double_value = 548658 WHERE id = 100;
+
+DELETE FROM sandbox WHERE id = 125;
+
+SELECT * FROM sandbox;
